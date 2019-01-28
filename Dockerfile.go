902a9c5bb157a7a21ce6a9ddf704c0d273ef728a
DQNEO <dqneoo@gmail.com>
2019-01-28 21:05:18 +0900

    Revert "Upgrade git to v1.11"
    This reverts commit e6e18f745983ad2c8676b6dbd1ef55e382e4f838.

--- Dockerfile.go
+++ Dockerfile.go
@@ -3,7 +3,7 @@ FROM dqneo/ubuntu-build-essential
 MAINTAINER DQNEO
 
 RUN \
-    apt-get -y install golang-1.11 \
+    apt-get -y install golang-1.10 \
     && apt-get clean
 
-RUN echo 'export PATH="/usr/lib/go-1.11/bin:$PATH"' >> /etc/bash.bashrc
+RUN echo 'export PATH="/usr/lib/go-1.10/bin:$PATH"' >> /etc/bash.bashrc
