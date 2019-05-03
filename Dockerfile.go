0b33d3d85cdb2396959834b1fca908edc6bc7078
DQNEO <dqneoo@gmail.com>
2019-05-03 22:45:06 +0900

    fix

--- Dockerfile.go
+++ Dockerfile.go
@@ -4,7 +4,7 @@ MAINTAINER DQNEO
 
 RUN \
     apt-get -y update \
-    apt-get -y install golang-1.10 \
+    && apt-get -y install golang-1.10 \
     && apt-get clean
 
 RUN echo 'export PATH="/usr/lib/go-1.10/bin:$PATH"' >> /etc/bash.bashrc
